6b97561723294dcc577ecd8849e2002d4f268fed
itambu <ivan.ignatenko@gmail.com>
2015-04-25 22:50:58 +0300

    test

---
+++ Mediateka/ConsoleApplication44/Season.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication44
+{
+    public class Season : ICollection<IMovieItem>
+    {
+        public TimeSpan Duration { get; set; }
+        public DateTime CreationDate { get; set; }
+        public Rating Rating { get; set; }
+        public string Name { get; set; }
+
+       // private ICollection<Mo
+
+        public void Add(IMovieItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(IMovieItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(IMovieItem[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool Remove(IMovieItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<IMovieItem> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Mediateka/ConsoleApplication44/Serial.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication44
+{
+    public class Serial : Movie, ICollection<Season>
+    {
+        private ICollection<Season> seasons = new List<Season>();
+
+        public void Add(Season item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(Season item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(Season[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool Remove(Season item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<Season> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
