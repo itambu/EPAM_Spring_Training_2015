06729de4754c2dc2c539c7ef88240172b5084cea
itambu <ivan.ignatenko@gmail.com>
2015-05-21 21:33:09 +0300

    Linq Example

---
+++ LinqExample/ConsoleApplication49/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication49
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ LinqExample/ConsoleApplication49/OrderedItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication49
+{
+    public class OrderedItem
+    {
+        public Item Item { get; set; }
+        public double Cost { get; set; }
+        public double Quantity { get; set; }
+        public double Total
+        {
+            get { return this.Cost * this.Quantity; }
+        }
+    }
+}
