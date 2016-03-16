e48554b72c3b67569f08106e39405ae962a54900
itambu <ivan.ignatenko@gmail.com>
2016-03-16 21:07:31 +0300

    Mediateka2

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextReader reader = null;
+            try
+            {
+                reader = File.OpenText(@"d:\stdafx1.h");
+                string s = null;
+                while ((s = reader.ReadLine()) != null)
+                {
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error");
+                throw new Exception("Error", e);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Dispose();
+                }
+            }
+
+            
+
+        }
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApplication2
+{
+    public class MyCollection<T> : IEnumerable<T>
+    {
+        private ICollection<T> collection;
+
+        public MyCollection(ICollection<T> source)
+        {
+            collection = source;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            List<T> temp = new List<T>();
+            foreach (var item in collection)
+            {
+                if (!temp.Contains(item))
+                {
+                    temp.Add(item);
+                    yield return item; 
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public IEnumerable<T> UserIterator()
+        {
+            List<T> temp = new List<T>();
+            foreach (var item in collection)
+            {
+                if (!temp.Contains(item))
+                {
+                    temp.Add(item);
+                    yield return item;
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            MyCollection<int> ilist = new MyCollection<int>(new List<int>() { 7, 1, 2, 3, 2, 4, 1, 6, 6, 3, 7 });
+
+            foreach (var item in ilist.UserIterator())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
