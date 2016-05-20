6434c8f179ba0f646750c97919d7fe3cceda2374
itambu <ivan.ignatenko@gmail.com>
2016-05-20 19:55:44 +0300

    threading

--- BlogExample.Model/BlogExample.Manager/BlogManager.cs
+++ BlogExample.Model/BlogExample.Manager/BlogManager.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.Collections.Concurrent;
 
 
 namespace BlogExample.Manager
 {
     public class BlogManager : IDisposable
     {
+        
         private System.IO.FileSystemWatcher _watcher;
+
+        private BlockingCollection<CancellationToken> cancelTokenCollection = new BlockingCollection<CancellationToken>(); 
 
         public BlogManager(System.IO.FileSystemWatcher watcher)
         {
@@ -19,7 +25,77 @@ namespace BlogExample.Manager
 
         public void OnWatcherCreated(object sender, System.IO.FileSystemEventArgs e)
         {
+            try
+            {
+                CancellationToken token = new CancellationToken();
+
+                Action createTaskAction = () =>
+                {
+                    using (var stream = new FileStream(e.FullPath, FileMode.Open, FileAccess.Read, FileShare.None))
+                    {
+                        using (var reader = new StreamReader(stream))
+                        {
+                            ParseCsv(reader, token);
+                        }
+                    }
+                };
+
+                var task = new Task(createTaskAction, token);
+                cancelTokenCollection.Add(token);
+
+                task.ContinueWith((sourceTask) =>
+                    {
+                        cancelTokenCollection.Take(token);
+                    });
+
+                task.Start();
+
+            }
+            finally
+            {
+                // Sql Server 2008 R2
+            }
+
+        }
+
+        protected void ParseCsv(System.IO.TextReader reader, CancellationToken token)
+        {
             
+            try
+            {
+                string current = null;
+                while ((current = reader.ReadLine()) != null && !token.IsCancellationRequested)
+                {
+                    // Parse string
+                    // add records to data context
+
+                    // add Order
+
+
+                }
+
+                if (current != null && token.IsCancellationRequested)
+                {
+                    // Rollback
+                }
+            }
+            catch (IOException e)
+            {
+                //Rollback
+            }
+
+            
+        }
+
+        private object orderSyncObj = new object();
+
+        public void Add(Order order)
+        {
+            // create repository
+            lock (orderSyncObj)
+            {
+
+            }
         }
 
         public void Run()
