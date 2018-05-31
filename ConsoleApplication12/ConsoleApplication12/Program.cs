ab7e08ce1a86995017f4d8cbc7eb947d62c24bcf
itambu <ivan.ignatenko@gmail.com>
2018-05-31 20:51:50 +0300

    EF Examle saledbs project

---
+++ ConsoleApplication12/ConsoleApplication12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine( ConfigurationManager.AppSettings["filePath"]);
+        }
+    }
+}
---
+++ ConsoleApplication13/ConsoleApplication13/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (CancellationTokenSource tokenSource = new CancellationTokenSource())
+            {
+                Task task = Task.Factory.StartNew(ThreadFunc, tokenSource, tokenSource.Token);
+                Thread.Sleep(1000);
+                tokenSource.Cancel();
+            }
+        }
+
+
+        static void ThreadFunc(object param)
+        {
+            try
+            {
+                int count = 100;
+                int index = 0;
+                while (index++ < count)
+                {
+                    Thread.Sleep(100);
+                    if (param != null && (param as CancellationTokenSource).IsCancellationRequested)
+                    {
+                    }
+                }
+            }
+            catch (ThreadAbortException e)
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+
+
+}
