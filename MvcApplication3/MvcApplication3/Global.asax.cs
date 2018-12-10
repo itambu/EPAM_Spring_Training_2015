78f91636efec449665011f7100232b503a137925
itambu <ivan.ignatenko@gmail.com>
2018-12-10 10:29:15 +0300

    EFProject

--- MvcApplication3/MvcApplication3/Global.asax.cs
+++ MvcApplication3/MvcApplication3/Global.asax.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-namespace MvcApplication1
+namespace MvcApplication3
 {
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
@@ -16,8 +16,6 @@ namespace MvcApplication1
     {
         protected void Application_Start()
         {
-            //System.Diagnostics.Debug.WriteLine("your message here"); 
-
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
